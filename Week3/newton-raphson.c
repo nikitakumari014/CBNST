86f043f70a2124d86b1af1352244cfcb69828808
NikitaKumari <nikita00413k@gmail.com>
2022-10-03 05:42:29 +0530

    First Commit

--- Week3/newton-raphson.c
+++ Week3/newton-raphson.c
@@ -1,26 +1,31 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 float f(float x)
 {
-    return pow(x, 2) - 5;
+    return x*log10(x) - 1.2;
 }
-float derivative(float x)
+float df (float x)
 {
-    return 2 * x;
+    return log10(x) + 0.43429;
 }
 int main()
 {
-    float x;                 // approximation
-    unsigned short i = 1, n; // number of iterations
-    printf("First approximation: ");
-    scanf("%f", &x);
-    printf("Number of iterations: ");
-    scanf("%hu", &n);
-    while (i <= n)
+    int itr, maxmitr;
+    float h, x0, x1, allerr;
+    printf("\nEnter x0, allowed error and maximum iterations\n");
+    scanf("%f %f %d", &x0, &allerr, &maxmitr);
+    for (itr=1; itr<=maxmitr; itr++)
     {
-        x = x - f(x) / derivative(x);
-        i++;
+        h=f(x0)/df(x0);
+        x1=x0-h;
+        printf(" At Iteration no. %3d, x = %9.6f\n", itr, x1);
+        if (fabs(h) < allerr)
+        {
+            printf("After %3d iterations, root = %8.6f\n", itr, x1);
+            return 0;
+        }
+        x0=x1;
     }
-    printf("Approximation root: %f \n", x);
-    return 0;
+    printf(" The required solution does not converge or iterations are insufficient\n");
+    return 1;
 }
